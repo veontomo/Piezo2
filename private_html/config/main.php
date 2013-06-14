762f7f5ec90b8eb98d481d003e7b1eb5c85033ff
Mario Rossi <veontomo@gmail.com>
2013-06-14 23:22:40 +0200

    new passwords

--- private_html/config/main.php
+++ private_html/config/main.php
@@ -47,9 +47,6 @@ return array(
 			),
 		),
 		*/
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
 		// uncomment the following to use a MySQL database
 
 		'db'=>array(
