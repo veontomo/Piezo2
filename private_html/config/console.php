2083ead3bc069a6c0aac7639c0e0b36d5777064f
Mario Rossi <veontomo@gmail.com>
2013-09-02 17:21:57 +0200

    configuration for test and development environments are moved to separate files, console.php is configured to use test database as a scratch

--- private_html/config/console.php
+++ private_html/config/console.php
@@ -11,17 +11,19 @@ return array(
 
 	// application components
 	'components'=>array(
+		'db'=>require(dirname(__FILE__) . '/dev_db.php'),
+		'testDb'=>require(dirname(__FILE__) . '/test_db.php'),
 //		'db'=>array(
 //			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 //		),
 		// uncomment the following to use a MySQL database
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=piezonuc_biblio',
-			'emulatePrepare' => true,
-			'username' => 'piezonuc_a1',
-			'password' => 'vYk*n4A',
-			'charset' => 'utf8',
-		),
+		// 'db'=>array(
+		// 	'connectionString' => 'mysql:host=localhost;dbname=piezonuc_biblio',
+		// 	'emulatePrepare' => true,
+		// 	'username' => 'piezonuc_a1',
+		// 	'password' => 'vYk*n4A',
+		// 	'charset' => 'utf8',
+		// ),
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
